a43fe125e5aa1da9fea66fcfc89675f1bb17cb20
elisavetTriant <elitriant@gmail.com>
2023-04-10 21:45:00 +0300

    First Prototype
    -SCSS
    -Data source (file)
    -Initial APP
    -Various Removals

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import imageData from './data/images.json';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="gallery_wrapper slider-container">
+        <ul className="inner-slider">
+          {imageData.children.map((imageSlide, index) => {
+            return (
+              <li className="card" key={index}>
+                <img src={imageSlide.children[0].src} alt={imageSlide.children[0].alt} />{imageSlide.children[1]}
+              </li>
+            )
+          })}
+        </ul>
+        <div className="indicatorsList">
+          {
+            imageData.children.map((imageSlideIndicator, index) => {
+              return (
+                <button className="indicator" key={index}>{index+1}</button>
+              )
+            })}
+        </div >
+      </div>
+    </>
   );
 }
 
