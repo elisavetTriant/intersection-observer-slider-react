4daedfaeb19444a73e240eddcf7c90f58d140d96
elisavetTriant <elitriant@gmail.com>
2023-04-18 19:33:37 +0300

    Add mouse enabled functionality

--- src/App.js
+++ src/App.js
@@ -3,10 +3,55 @@ import imageData from './data/images.json';
 
 function App() {
 
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  const handleMouseEnter = function (e) {
+    isDown = false;
+    e.currentTarget.style.cursor = "grab";
+  };
+
+  const handleMouseDown = function (e) {
+    isDown = true;
+    e.currentTarget.classList.add("active");
+    startX = e.pageX - e.currentTarget.offsetLeft;
+    scrollLeft = e.currentTarget.scrollLeft;
+    e.currentTarget.style.cursor = "grabbing";
+  };
+
+  const handleMouseLeave = function (e) {
+    isDown = false;
+    e.currentTarget.classList.remove("active");
+    e.currentTarget.style.cursor = "default";
+  };
+
+  const handleMouseUp = function (e) {
+    isDown = false;
+    e.currentTarget.classList.remove("active");
+    e.currentTarget.style.cursor = "grab";
+  };
+
+  const handleMouseMove = function (e) {
+    if (!isDown) return;
+    e.currentTarget.style.cursor = "grabbing";
+    e.preventDefault();
+    const x = e.pageX - e.currentTarget.offsetLeft;
+    const SCROLL_SPEED = 3;
+    const walk = (x - startX) * SCROLL_SPEED;
+    e.currentTarget.scrollLeft = scrollLeft - walk;
+  };
+
   return (
     <>
       <div className="gallery_wrapper slider-container">
-        <ul className="inner-slider">
+        <ul className="inner-slider"
+          onMouseEnter={(e) => handleMouseEnter(e)}
+          onMouseDown={(e) => handleMouseDown(e)}
+          onMouseLeave={(e) => handleMouseLeave(e)}
+          onMouseUp={(e) => handleMouseUp(e)}
+          onMouseMove={(e) => handleMouseMove(e)}
+        >
           {imageData.children.map((imageSlide, index) => {
             return (
               <li className="card" key={index}>
@@ -19,7 +64,7 @@ function App() {
           {
             imageData.children.map((imageSlideIndicator, index) => {
               return (
-                <button className="indicator" key={index}>{index+1}</button>
+                <button className="indicator" key={index}>{index + 1}</button>
               )
             })}
         </div >
